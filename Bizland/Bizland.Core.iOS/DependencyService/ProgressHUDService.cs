c7dbbc9a5767845a56e57864acb19ea061590d61
Tran Hoang Nam (PPM) <namth2@bagroup.vn>
2020-02-10 15:39:01 +0700

    doing

--- Bizland/Bizland.Core.iOS/DependencyService/ProgressHUDService.cs
+++ Bizland/Bizland.Core.iOS/DependencyService/ProgressHUDService.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Bizland.Core.iOS.DependencyService;
-using CoreAnimation;
-using Foundation;
+﻿using Bizland.Core.iOS.DependencyService;
+using Bizland.Core.iOS.Extensions;
+
 using Ricardo.RMBProgressHUD.iOS;
+
 using UIKit;
+
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(ProgressHUDService))]
+
 namespace Bizland.Core.iOS.DependencyService
 {
     public class ProgressHUDService : IProgressHUDService
     {
-        MBProgressHUD hud;
+        private MBProgressHUD hud;
+
         public ProgressHUDService()
         {
         }
@@ -34,25 +34,16 @@ namespace Bizland.Core.iOS.DependencyService
 
             // The sample image is based on the work by http://www.pixelpressicons.com, http://creativecommons.org/licenses/by/2.5/ca/
             // Make the customViews 37 by 37 pixels for best results (those are the bounds of the build-in progress indicators)
-            hud.CustomView = new UIImageView(UIImage.FromBundle("logoprosess.png"));
-
-
-            CABasicAnimation rotation;
-            rotation = new CABasicAnimation();
-            rotation.KeyPath = "transform.rotation";
-            rotation.Duration = 0.7f;
-            rotation.RemovedOnCompletion = false;
-            hud.CustomView.Layer.AddAnimation(rotation, "Spin");
+            UIImageView gif = AnimatedImageView.GetAnimatedImageView("https://i.stack.imgur.com/naLBK.gif");
+            hud.CustomView = gif;
             // Add information to your HUD
             hud.Label.Text = message;
             hud.BezelView.Color = UIColor.Clear;
             hud.BezelView.TintColor = UIColor.Clear;
-            hud.BezelView.Style =MBProgressHUDBackgroundStyle.SolidColor;
+            hud.BezelView.Style = MBProgressHUDBackgroundStyle.SolidColor;
             hud.BezelView.BlurEffectStyle = UIBlurEffectStyle.Dark;
             // Set custom view mode
             hud.Mode = MBProgressHUDMode.CustomView;
-            rotation.RepeatCount = 500f;
-          
 
             hud.Show(true);
         }

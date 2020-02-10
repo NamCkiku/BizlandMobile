1edb3838400f22027f47ef9cc9ae44ca805ed36a
Tran Hoang Nam (PPM) <namth2@bagroup.vn>
2020-02-10 15:18:11 +0700

    doing

--- Bizland/Bizland.Core.iOS/DependencyService/ProgressHUDService.cs
+++ Bizland/Bizland.Core.iOS/DependencyService/ProgressHUDService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Bizland.Core.iOS.DependencyService;
+using CoreAnimation;
 using Foundation;
 using Ricardo.RMBProgressHUD.iOS;
 using UIKit;
@@ -30,15 +31,28 @@ namespace Bizland.Core.iOS.DependencyService
         UIApplication.SharedApplication.KeyWindow.RootViewController;
             hud = new MBProgressHUD(controller.View);
             controller.View.AddSubview(hud);
-            hud.ContentColor = UIColor.Clear;
-            // Add information to your HUD
-            hud.Label.Text = message;
-            // Set custom view mode
-            hud.Mode = MBProgressHUDMode.CustomView;
-            hud.Margin = 0f;
+
             // The sample image is based on the work by http://www.pixelpressicons.com, http://creativecommons.org/licenses/by/2.5/ca/
             // Make the customViews 37 by 37 pixels for best results (those are the bounds of the build-in progress indicators)
             hud.CustomView = new UIImageView(UIImage.FromBundle("logoprosess.png"));
+
+
+            CABasicAnimation rotation;
+            rotation = new CABasicAnimation();
+            rotation.KeyPath = "transform.rotation";
+            rotation.Duration = 0.7f;
+            rotation.RemovedOnCompletion = false;
+            hud.CustomView.Layer.AddAnimation(rotation, "Spin");
+            // Add information to your HUD
+            hud.Label.Text = message;
+            hud.BezelView.Color = UIColor.Clear;
+            hud.BezelView.TintColor = UIColor.Clear;
+            hud.BezelView.Style =MBProgressHUDBackgroundStyle.SolidColor;
+            hud.BezelView.BlurEffectStyle = UIBlurEffectStyle.Dark;
+            // Set custom view mode
+            hud.Mode = MBProgressHUDMode.CustomView;
+            rotation.RepeatCount = 500f;
+          
 
             hud.Show(true);
         }
